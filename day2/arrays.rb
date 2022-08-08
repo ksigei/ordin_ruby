bac399fd051efae9bff5f7a8bc083f5458b2902c
“ksigei” <sigeikiprono4@gmail.com>
2022-08-08 12:10:45 +0300

    Arrays

---
+++ day2/arrays.rb
@@ -0,0 +1,29 @@
+arr1 = [1, 2, 3, 4, 5]
+# puts arr1
+# use print if you dont need to return items in new lines
+# print arr1 
+# adding to the end
+# arr2 = arr1.push(2)
+arr2 = arr1 << 2
+# puts arr2
+
+# to the beginning
+arr3 = arr1.unshift(3)
+# puts arr3
+
+# to specific position
+arr4 =  arr1.insert(2, 30)
+# puts arr4
+
+# fetch
+# first 
+# last 
+# take - returns the first n elements of an array
+
+# others
+# drop - return elements after n object has been dropped
+# pop - removes from the end
+# shift - removes from the start
+# delete - removes specified item
+# uniq - removes duplicate items
+
