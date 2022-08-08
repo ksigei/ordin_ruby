0159dab17c404a815659362160b2750236a9b1bf
“ksigei” <sigeikiprono4@gmail.com>
2022-08-08 12:23:01 +0300

    Hashes

---
+++ day2/hashes.rb
@@ -0,0 +1,11 @@
+#storing variables using keys and values
+# you ca use either => or :
+
+# hash1 = {Item : "Jug", Price: "100", Size: "2.5"}
+hash2 = {"Item" => "Jug", "Price" => 100, "Size" => 2.5}
+puts hash2["Price"]
+puts hash2["Size"]
+# puts hash1.size
+# puts hash2.size
+a = "Item"
+puts hash2.include? a
